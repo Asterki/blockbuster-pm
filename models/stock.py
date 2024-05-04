e5bb5192b6edaaa55934ff82135a8ec473d22356
asterki <asterki.dev@proton.me>
2024-05-04 17:54:04 -0600

    Feat: Inventory Model

--- models/stock.py
+++
@@ -1,8 +0,0 @@
-from services.database import DatabaseService
-
-
-class StockModel:
-    def __init__(self):
-        self.db = DatabaseService().get_instance()
-
-    def create_stock(self, name, price, stock):
